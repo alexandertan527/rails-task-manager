ffc75c3f9a839a057f325c9eb81ceef9e8238a0c
Alexander Tan <alexandertan527@gmail.com>
2025-02-08 16:54:20 +0000

    committing models/controllers/views so far

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: 'task'
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: 'edit'
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
